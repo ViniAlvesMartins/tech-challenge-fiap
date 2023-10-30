3603d759358f321e214acc0a5f6cb7e7b352c0e8
Marcelo Filho <marcelo.rodriguespires@gmail.com>
2023-10-30 12:20:40 -0300

    Refactoring dto validation

--- src/adapter/inbound/dto/client_dto.go
+++ src/adapter/inbound/dto/client_dto.go
@@ -1,10 +1,7 @@
 package dto
 
 import (
-	"errors"
-	"fmt"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/core/domain/entity"
-	"github.com/go-playground/validator/v10"
 )
 
 type ClientDto struct {
@@ -14,52 +11,11 @@ type ClientDto struct {
 	Email string `json:"email" validate:"required" error:"Campo email é obrigatorio"`
 }
 
-type IValidateErrorClient struct {
-	Errors []Fields
-}
-
-type ClientFields struct {
-	Field   string
-	Message string
-}
-
-func ValidateClient(dto ClientDto) IValidateError {
-
-	validate = validator.New(validator.WithRequiredStructEnabled())
-
-	err := validate.Struct(dto)
-
-	var errList []Fields
-
-	if err != nil {
-		var invalidValidationError *validator.InvalidValidationError
-		if errors.As(err, &invalidValidationError) {
-			fmt.Println(err)
-		}
-
-		for _, err := range err.(validator.ValidationErrors) {
-			errList = append(errList, Fields{
-				Field:   err.Field(),
-				Message: err.Param(),
-			})
-		}
+func (c *ClientDto) ConvertEntity() entity.Client {
+	return entity.Client{
+		ID:    c.ID,
+		Name:  c.Name,
+		Cpf:   c.Cpf,
+		Email: c.Email,
 	}
-
-	var validateError IValidateError
-
-	validateError.Errors = errList
-
-	return validateError
-}
-
-func ConvertClientDtoToDomain(dto ClientDto) entity.Client {
-
-	var client = entity.Client{
-		ID:    dto.ID,
-		Name:  dto.Name,
-		Cpf:   dto.Cpf,
-		Email: dto.Email,
-	}
-
-	return client
 }
--- src/adapter/inbound/dto/product_dto.go
+++ src/adapter/inbound/dto/product_dto.go
@@ -1,9 +1,7 @@
 package dto
 
 import (
-	"fmt"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/core/domain/entity"
-	"github.com/go-playground/validator/v10"
 )
 
 type ProductDto struct {
@@ -15,52 +13,13 @@ type ProductDto struct {
 	Active      bool    `json:"active"`
 }
 
-var validate *validator.Validate
-
-type IValidateError struct {
-	Errors []Fields
-}
-
-type Fields struct {
-	Field   string
-	Message string
-}
-
-func ValidateProduct(dto ProductDto) IValidateError {
-	var validateError IValidateError
-	validate = validator.New(validator.WithRequiredStructEnabled())
-
-	err := validate.Struct(dto)
-
-	var errList []Fields
-
-	if err != nil {
-		if _, ok := err.(*validator.InvalidValidationError); ok {
-			fmt.Println(err)
-		}
-
-		for _, err := range err.(validator.ValidationErrors) {
-			errList = append(errList, Fields{
-				Field:   err.Field(),
-				Message: err.Param(),
-			})
-		}
+func (p *ProductDto) ConvertToEntity() entity.Product {
+	return entity.Product{
+		ID:          p.ID,
+		NameProduct: p.NameProduct,
+		Description: p.Description,
+		Price:       p.Price,
+		CategoryId:  p.CategoryId,
+		Active:      p.Active,
 	}
-
-	validateError.Errors = errList
-
-	return validateError
-}
-
-func ConvertDtoToDomain(dto ProductDto) entity.Product {
-	var product = entity.Product{
-		ID:          dto.ID,
-		NameProduct: dto.NameProduct,
-		Description: dto.Description,
-		Price:       dto.Price,
-		CategoryId:  dto.CategoryId,
-		Active:      dto.Active,
-	}
-
-	return product
 }
