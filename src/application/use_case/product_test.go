1f516210127989d696a94455cc4b8ba2b07cfd2f
Marcelo Filho <marcelo.rodriguespires@gmail.com>
2024-04-23 15:21:52 -0300

    Adding test coverage command

--- src/application/use_case/product_test.go
+++ src/application/use_case/product_test.go
@@ -1,6 +1,7 @@
 package use_case
 
 import (
+	"errors"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/application/contract/mock"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/entities/entity"
 	"github.com/golang/mock/gomock"
@@ -27,9 +28,31 @@ func TestProductUseCase_Create(t *testing.T) {
 		repo.EXPECT().Create(p).Return(p, nil).Times(1)
 
 		productUseCase := NewProductUseCase(repo, logger)
-
 		product, err := productUseCase.Create(p)
+
 		assert.Nil(t, err)
 		assert.Equal(t, p, *product)
+	})
+
+	t.Run("repository error creating procuct", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+		expectedErr := errors.New("error connecting to database")
+
+		p := entity.Product{
+			NameProduct: "Test product",
+			Description: "Product created to test",
+			Price:       123.45,
+			CategoryId:  1,
+			Active:      true,
+		}
+
+		repo := mock.NewMockProductRepository(ctrl)
+		repo.EXPECT().Create(p).Return(p, expectedErr).Times(1)
+
+		productUseCase := NewProductUseCase(repo, logger)
+		_, err := productUseCase.Create(p)
+
+		assert.ErrorIs(t, err, expectedErr)
 	})
 }
