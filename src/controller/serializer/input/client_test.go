14bfdc1e80fe69e9ee0f2dcb337c56774a8f748c
Marcelo Filho <marcelo.rodriguespires@gmail.com>
2024-05-20 15:21:13 -0300

    Adding serializers tests

---
+++ src/controller/serializer/input/client_test.go
@@ -0,0 +1,19 @@
+package input
+
+import (
+	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/entities/entity"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestClientDto_ConvertEntity(t *testing.T) {
+	t.Run("convert dto to entity successfully", func(t *testing.T) {
+		c := ClientDto{
+			Cpf:   12345678911,
+			Name:  "Test Client",
+			Email: "testclient@example.com",
+		}
+
+		assert.IsType(t, entity.Client{}, c.ConvertEntity())
+	})
+}
---
+++ src/controller/serializer/input/product_test.go
@@ -0,0 +1,20 @@
+package input
+
+import (
+	"github.com/ViniAlvesMartins/tech-challenge-fiap/src/entities/entity"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProductDto_ConvertToEntity(t *testing.T) {
+	t.Run("convert dto to entity successfully", func(t *testing.T) {
+		dto := ProductDto{
+			NameProduct: "Test Product",
+			Description: "Test created for test purposes",
+			Price:       123.45,
+			CategoryId:  1,
+		}
+
+		assert.IsType(t, entity.Product{}, dto.ConvertToEntity())
+	})
+}
